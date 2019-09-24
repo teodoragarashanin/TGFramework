1603f86eaa0e98177b615ae32b038c64e50c7819
Teodora Garasanin <t.garasanin@levi9.com>
2019-09-24 14:24:41 +0200

    Initial commit

---
+++ TGFramework/TGFramework/TGFramework.h
@@ -0,0 +1,19 @@
+//
+//  TGFramework.h
+//  TGFramework
+//
+//  Created by Teodora Garasanin on 9/24/19.
+//  Copyright © 2019 Teodora Garasanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TGFramework.
+FOUNDATION_EXPORT double TGFrameworkVersionNumber;
+
+//! Project version string for TGFramework.
+FOUNDATION_EXPORT const unsigned char TGFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGFramework/PublicHeader.h>
+
+
---
+++ TGFramework/TGFramework/WebViewWrapper.swift
@@ -0,0 +1,24 @@
+//
+//  WebViewWrapper.swift
+//  TGFramework
+//
+//  Created by Teodora Garasanin on 9/24/19.
+//  Copyright © 2019 Teodora Garasanin. All rights reserved.
+//
+
+import Foundation
+
+public class WebViewWrapper {
+    
+    private init() {}
+    
+    public static func showWebView(storyboard: UIStoryboard) {
+        let webView = WKWebView(frame: UIScreen.main.bounds)
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        let vc = storyboard.instantiateInitialViewController()
+        vc?.view.addSubview(webView)
+    }
+}
